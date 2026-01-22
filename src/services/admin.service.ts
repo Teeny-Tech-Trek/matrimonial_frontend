03fb8cc72a7fbe045a547a828a55fb930ff3ab6a
dhruvmishra482 <dhruvmishra3829@gmail.com>
2026-01-22 17:20:47 +0530

    changes

--- src/services/admin.service.ts
+++ src/services/admin.service.ts
@@ -5,6 +5,8 @@ import axios from 'axios';
 const API_BASE_URL = 'https://api.rsaristomatch.com/api';
 // const API_BASE_URL = 'http://localhost:5000/api';
 
+
+
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
   headers: {
