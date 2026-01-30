9d5731031667816c051fea68123d5c4c0a92bd55
dhruvmishra482 <dhruvmishra3829@gmail.com>
2026-01-30 18:00:30 +0530

    routing

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 const api = axios.create({
     // baseURL: import.meta.env.VITE_API_URL,
     // baseURL:"http://localhost:5000/api" ,
-    baseURL:   "https://www.rsaristomatch.com", 
+    baseURL:"https://api.rsaristomatch.com" ,
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
